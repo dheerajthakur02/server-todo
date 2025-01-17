8ebbd2855ad87b6091378e4d22abc277c9b1072b
Dheeraj Kumar Thalur <114847997+dheerajthakur02@users.noreply.github.com>
2025-01-17 22:18:40 +0530

    Update Todo.js

--- Models/Todo.js
+++ Models/Todo.js
@@ -1,13 +1,13 @@
-const mongoose= require("mongoose");
+import mongoose from "mongoose";
 
-const TodoSchema= new mongoose.Schema({
+const TodoSchema = new mongoose.Schema({
     task: String,
     done: {
         type: Boolean,
         default: false
     }
-})
+});
 
-const TodoModel= mongoose.model("todos",TodoSchema)
+const TodoModel = mongoose.model("todos", TodoSchema);
 
-module.exports = TodoModel
+export default TodoModel;
